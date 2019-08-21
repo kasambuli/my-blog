9ef8b4c686377d544f768486429185fd771631c5
kasambuli <cynthiakasambuli@gmail.com>
2019-08-21 19:04:56 +0300

    Initial commit

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Post {
+    private String content;
+
+    public Post(String content){
+        this.content = content;
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+    }
+}

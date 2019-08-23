790c90b94bed7ad879998d703dc4b74be832f1f9
kasambuli <cynthiakasambuli@gmail.com>
2019-08-23 13:39:37 +0300

    Created the ArrayList for the post and displayed them

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,14 +1,26 @@
 package models;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Post {
     private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
 
     public Post(String content){
         this.content = content;
-
+        instances.add(this);
     }
 
     public String getContent() {
         return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear();
     }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -14,11 +14,26 @@ public class PostTest {
 
     @After
     public void tearDown() throws Exception {
+        Post.clearAllPosts();//clears out all the posts before each test
     }
 
     @Test
     public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
         Post post = new Post("Day 1: Intro");
         assertEquals("Day 1: Intro", post.getContent());
+    }
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
     }
 }
